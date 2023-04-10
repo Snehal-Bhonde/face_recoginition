0d5bbafebdcc862e0d058ccd38fcc2b0a06811ba
Admin <snehal.bhonde@atriina.com>
2023-04-10 20:26:09 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/face_recoginition/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.face_recoginition
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+import 'face_recognition/FaceDetectorView.dart';
+
+void main() {
+  runApp(MaterialApp(
+      home:FaceDetectorView()
+  ));
+}
